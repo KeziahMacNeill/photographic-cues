c32a984bf9dd586d79929581a285c3f43bc28e12
Mickey van Olst <post@mickeyvanolst.com>
2022-07-24 23:40:14 +0200

    styling the mobile page a bit and adding a share button

--- js/ops.js
+++ js/ops.js
@@ -32491,6 +32491,157 @@ Ops.Browser.BrowserInfo_v3.prototype = new CABLES.Op();
 CABLES.OPS["ec3e0121-b2c2-4c31-bbda-a6982080f73f"]={f:Ops.Browser.BrowserInfo_v3,objName:"Ops.Browser.BrowserInfo_v3"};
 
 
+
+
+// **************************************************************
+// 
+// Ops.Browser.WebShare
+// 
+// **************************************************************
+
+Ops.Browser.WebShare = function()
+{
+CABLES.Op.apply(this,arguments);
+const op=this;
+const attachments={};
+const
+    inText = op.inString("Text", "check out undev"),
+    inUrl = op.inString("URL", "https://undev.studio"),
+    inBase64 = op.inString("Base64 File"),
+    inDataUrl = op.inString("Data URL"),
+    inFileType = op.inString("Filetype", "image/png"),
+    inFileName = op.inString("Filename", "screenshot.png"),
+    exec = op.inTriggerButton("Share"),
+    outStatus = op.outString("Status", "none"),
+    outSupport = op.outBoolNum("Supported", !!navigator.share);
+
+op.setPortGroup("File", [inBase64, inDataUrl, inFileType, inFileName]);
+
+if (!navigator.share)
+{
+    op.setUiError("noShare", "webshare api not supported on this device", 1);
+}
+
+exec.onTriggered = () =>
+{
+    op.setUiError("noShare", null);
+    if (!navigator.share)
+    {
+        op.setUiError("noShare", "webshare api not supported on this device", 1);
+        outSupport.set(false);
+        return;
+    }
+    outSupport.set(true);
+
+    const shareData = {};
+    shareData.text = inText.get();
+    shareData.url = inUrl.get();
+
+    let url = inDataUrl.get();
+    if (inBase64.get())
+    {
+        url = "data:" + inFileType.get() + ";base64," + inBase64.get();
+    }
+
+    if (url)
+    {
+        fetch(url)
+            .then((res) => res.blob())
+            .then((blob) =>
+            {
+                shareData.files = [
+                    new File(
+                        [blob],
+                        inFileName.get(),
+                        {
+                            "type": inFileType.get(),
+                            "lastModified": new Date().getTime()
+                        }
+                    )
+                ];
+                if (!navigator.canShare(shareData))
+                {
+                    op.setUiError("noShare", "browser can't share files", 1);
+                    op.log("browser cant share files", shareData);
+                    delete shareData.files;
+                }
+                if (!navigator.canShare(shareData))
+                {
+                    op.setUiError("noShare", "browser can't share data", 1);
+                    op.log("browser cant share data", shareData);
+                    outStatus.set("error");
+                }
+                else
+                {
+                    doShare(shareData);
+                }
+            });
+    }
+    else
+    {
+        doShare(shareData);
+    }
+};
+
+function doShare(shareData)
+{
+    navigator.share(shareData)
+        .then(() =>
+        {
+            outStatus.set("success");
+        })
+        .catch((error) =>
+        {
+            outStatus.set("error");
+            op.setUiError("noShare", "Error sharing", error);
+            op.log("Error sharing", error);
+        });
+}
+
+
+};
+
+Ops.Browser.WebShare.prototype = new CABLES.Op();
+CABLES.OPS["2e24a18f-161a-480c-a86b-4232c59f68c4"]={f:Ops.Browser.WebShare,objName:"Ops.Browser.WebShare"};
+
+
+
+
+// **************************************************************
+// 
+// Ops.String.SwitchStringBoolean
+// 
+// **************************************************************
+
+Ops.String.SwitchStringBoolean = function()
+{
+CABLES.Op.apply(this,arguments);
+const op=this;
+const attachments={};
+const
+    inBool=op.inValueBool("Boolean"),
+    inStrTrue=op.inString("True","Yes"),
+    inStrFalse=op.inString("False","No"),
+    result=op.outString("Result");
+
+inBool.onChange=
+inStrFalse.onChange=
+inStrTrue.onChange=update;
+
+function update()
+{
+    if(inBool.get())result.set(inStrTrue.get());
+        else result.set(inStrFalse.get());
+}
+
+update();
+
+};
+
+Ops.String.SwitchStringBoolean.prototype = new CABLES.Op();
+CABLES.OPS["19e3c428-22ce-45a3-b903-fddfc46fc0a3"]={f:Ops.String.SwitchStringBoolean,objName:"Ops.String.SwitchStringBoolean"};
+
+
 window.addEventListener('load', function(event) {
 CABLES.jsLoaded=new Event('CABLES.jsLoaded');
 document.dispatchEvent(CABLES.jsLoaded);
